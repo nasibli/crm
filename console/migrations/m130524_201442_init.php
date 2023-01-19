3cc6994e54e8306b2d8ea9276bdef86ad92795cf
nasibli <glpat-z68Vzx3s9W5s94ykpiH1>
2023-01-19 12:19:01 +0300

    Развернул локально

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -14,6 +14,7 @@ class m130524_201442_init extends Migration
 
         $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
+            'role_id' => $this->integer()->null(),
             'username' => $this->string()->notNull()->unique(),
             'auth_key' => $this->string(32)->notNull(),
             'password_hash' => $this->string()->notNull(),
